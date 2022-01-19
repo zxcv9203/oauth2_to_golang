6e1ff7239edf473d38091f956f9c78785d33d521
zxcv9203 <zxcv9203@naver.com>
2022-01-20 07:30:00 +0900

    oauth2 readme

--- client/client.go
+++ client/client.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
 	"golang.org/x/oauth2"
 )
 
+/*
+	oauth2란?
+	구글, 페이스북, 카카오 등에서 제공하는 인증 서버를 통해
+	회원 정보를 인증하고 Access Token을 발급받기 위한 표준 프로토콜
+
+	발급받은 Access Token을 이용하여 인증 받은 곳(구글, 페이스북, 카카오 등)
+	의 API 서비스를 이용할 수 있게 됩니다.
+*/
+
+/*
+	oauth2 용어
+
+	Resource owner : Resource server로 부터 계정을 소유하고 있는 사용자를 의미합니다.
+
+	Client : 구글, 페이스북, 카카오 등의 API 서비스를 이용하는 제 3의 서비스를 의미합니다.
+
+	Authorization Server(권한 서버) : 권한을 관리해주는 서버, Access
+*/
 const (
 	authServerURL = "http://localhost:8080"
 )
 
+/*
+	config 각 변수 역할
+	ClientID : OAuth client
+*/
 var (
 	config = oauth2.Config{
-		ClientID:     "id",
-		ClientSecret: "pw",
+		ClientID:     "OAuthID-example",
+		ClientSecret: "OAuthSecret-example",
 		Scopes:       []string{"all"},
 		RedirectURL:  "http://localhost:8080/oauth2",
 		Endpoint: oauth2.Endpoint{
-			AuthURL:  authServerURL + "/authorize",
-			TokenURL: authServerURL + "/token",
+			AuthURL:  authServerURL + "/oauth/authorize",
+			TokenURL: authServerURL + "/oauth/token",
 		},
 	}
 	globalToken *oauth2.Token
@@ -26,15 +50,32 @@ var (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		u := config.AuthcodeURL("xyz",
+			oauth2.SetAuthURLParam("code_challenge", genCodeChallengeS256("s256example")),
+			oauth2.SetAuthURLParam("code_challenge_method", "S256"))
 		http.Redirect(w, r, u, http.StatusFound)
 	})
+
 	http.HandleFunc("/oauth2", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		state := r.Form.Get("state")
+		if state != "xyz" {
+			http.Error(w, "State invalid", http.StatusBadRequest)
+			return
+		}
 		code := r.Form.Get("code")
-		token, err :=
-			config.Exchange(context.Backgroun(),
-				code, oauth2.SetAuthURLParam("code_verifier",
-					"s256example"))
+		if code == "" {
+			http.Error(w, "Code not found", http.StatusBadRequest)
+			return
+		}
+		token, err := config.Exchange(context.Background(), code, oauth2.SetAuthURLParam("code_verifier", "s256example"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		globalToken = token
+
+		e := json.NewEncoder(w)
+		e.SetIndent
 	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,25 @@
 module github.com/zxcv9203/oauth_test
 
 go 1.17
+
+require (
+	github.com/go-oauth2/oauth2/v4 v4.4.2
+	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/tidwall/btree v0.0.0-20191029221954-400434d76274 // indirect
+	github.com/tidwall/buntdb v1.1.2 // indirect
+	github.com/tidwall/gjson v1.6.0 // indirect
+	github.com/tidwall/grect v0.0.0-20161006141115-ba9a043346eb // indirect
+	github.com/tidwall/match v1.0.1 // indirect
+	github.com/tidwall/pretty v1.0.1 // indirect
+	github.com/tidwall/rtree v0.0.0-20180113144539-6cd427091e0e // indirect
+	github.com/tidwall/tinyqueue v0.0.0-20180302190814-1e39f5511563 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
