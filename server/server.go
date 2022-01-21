3611b073903dc246ab576afee16748c2b34fc6e6
zxcv9203 <zxcv9203@naver.com>
2022-01-21 11:20:41 +0900

    server flag

--- server/server.go
+++ server/server.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/go-oauth2/oauth2/store"
+	"github.com/go-oauth2/oauth2/v4/manage"
+)
+
 var (
 	dumpvar   bool
 	idvar     string
@@ -8,4 +16,22 @@ var (
 	portvar   int
 )
 
-func init()
+func init() {
+	flag.BoolVar(&dumpvar, "d", true, "Dump requests and responses")
+	flag.StringVar(&idvar, "i", "OAuthID-example", "The Client id being passed in")
+	flag.StringVar(&secretvar, "s", "OAuthSecret-example", "The Client Secret being passed in")
+	flag.StringVar(&domainvar, "r", "http://localhost:8080", "The domain of the redirect URL")
+	flag.IntVar(&portvar, "p", 9096, "the base port for the server")
+}
+
+func main() {
+	flag.Parse()
+	if dumpvar {
+		log.Println("Dumping requests")
+	}
+	manager := manage.NewDefaultManager()
+	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+
+	// token store
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+}
